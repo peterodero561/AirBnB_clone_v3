f297723acd1e1f12cd96770e2fe2de38a0518317
peterodero561 <peterodero561@gmail.com>
2024-05-05 20:13:28 +0000

    feat: update

---
+++ web_dynamic/static/scripts/3-hbnb.js
@@ -0,0 +1,70 @@
+$(document).ready(function() {
+    // Array to store checked amenities
+    let checkedAmenities = [];
+
+    // Listen for changes on checkbox
+    $('.amenity-checkbox').change(function() {
+        let amenityID = $(this).parent().data('id');
+        let amenityName = $(this).parent().data('name');
+        
+        if (this.checked) {
+            checkedAmenities.push(amenityID);
+        } else {
+            let index = checkedAmenities.indexOf(amenityID);
+            if (index !== -1) {
+                checkedAmenities.splice(index, 1);
+            }
+        }
+
+        // Update the h4 tag with the list of checked amenities
+        $('#amenities-list').text(checkedAmenities.join(', '));
+    });
+
+    // Request API status
+    fetch('http://0.0.0.0:5001/api/v1/status/')
+        .then(response => response.json())
+        .then(data => {
+            // Check if status is "OK"
+            if (data.status === 'OK') {
+                // Add the class "available" to the api_status div
+                $('#api_status').addClass('available');
+
+		// Request places data
+                fetch('http://0.0.0.0:5001/api/v1/places_search/', {
+                    method: 'POST',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    body: JSON.stringify({})
+                })
+                .then(response => response.json())
+                .then(data => {
+                    // Loop through places data and create article tags
+                    data.forEach(place => {
+                        let article = $('<article>');
+                        let titleBox = $('<div>').addClass('title_box');
+                        let title = $('<h2>').text(place.name);
+                        let priceByNight = $('<div>').addClass('price_by_night').text('$' + place.price_by_night);
+                        titleBox.append(title, priceByNight);
+                        let information = $('<div>').addClass('information');
+                        let maxGuest = $('<div>').addClass('max_guest').text(place.max_guest + ' Guest' + (place.max_guest !== 1 ? 's' : ''));
+                        let numberRooms = $('<div>').addClass('number_rooms').text(place.number_rooms + ' Bedroom' + (place.number_rooms !== 1 ? 's' : ''));
+                        let numberBathrooms = $('<div>').addClass('number_bathrooms').text(place.number_bathrooms + ' Bathroom' + (place.number_bathrooms !== 1 ? 's' : ''));
+                        information.append(maxGuest, numberRooms, numberBathrooms);
+                        let description = $('<div>').addClass('description').html(place.description);
+                        article.append(titleBox, information, description);
+                        $('.places').append(article);
+                    });
+                })
+                .catch(error => {
+                    console.error('Error fetching places:', error);
+                });
+            } else {
+                // Remove the class "available" from the api_status div
+                $('#api_status').removeClass('available');
+            }
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+});
